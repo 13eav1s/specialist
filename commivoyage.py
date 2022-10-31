bc5697205b686b8c411b7af0feb607fe4f68139c
KrasnovLeonid <45150259+13eav1s@users.noreply.github.com>
2022-10-31 12:44:28 +0300

    Update commivoyage.py
    Find the minimum path length between all points

--- commivoyage.py
+++ commivoyage.py
@@ -1,41 +1,38 @@
+from itertools import combinations
+
+
 def path_length(a: dict, b: dict):
     return ((b['x'] - a['x']) ** 2 + (b['y'] - a['y']) ** 2) ** 0.5
 
 
-def find_next_point(current_point, arr, passed_points):
-    min_leght = 9999999
-    save_index = 0
-    for i in range(len(arr)):
-        if not arr[i] in passed_points:
-            current_lenght = path_length(current_point, arr[i])
-            if min_leght > current_lenght:
-                min_leght = current_lenght
-                save_index = i
-    passed_points.append(arr[save_index])
-    return min_leght
-
-
 def solution(arr):
-    passed_points = []
-    point = arr[0]
-    result = 0
-    passed_points.append(arr[0])
-    while len(passed_points) < len(arr):
-        result += find_next_point(point, arr, passed_points)
-        point = passed_points[len(passed_points) - 1]
-    return result
+    points = ''
+    for i in range(len(arr)):
+        points += str(i)
+    all_combinations = set(combinations(points*len(points), len(points)))
+    result_combinations = []
+    for i in all_combinations:
+        flag = 0
+        for j in points:
+            if i.count(j) != 1:
+                flag = 1
+                break
+        if flag == 0:
+            result_combinations.append(i)
+    min_length = 99999
+    for i in range(len(result_combinations)):
+        length = 0
+        for j in range(1, len(result_combinations[i])):
+            length += path_length(arr[int(result_combinations[i][j - 1])], arr[int(result_combinations[i][j])])
+        if min_length > length:
+            min_length = length
+    return min_length
 
 
 lst = [
-    {'x': 0, 'y': 1},
-    {'x': 0, 'y': 9},
+    {'x': 0, 'y': 0},
+    {'x': 0, 'y': -3},
     {'x': 0, 'y': 2},
-    {'x': 0, 'y': 5},
     {'x': 0, 'y': 3},
-    {'x': 0, 'y': 4},
-    {'x': 0, 'y': 6},
-    {'x': 0, 'y': 7},
-    {'x': 0, 'y': 8},
-
 ]
 print(solution(lst))
